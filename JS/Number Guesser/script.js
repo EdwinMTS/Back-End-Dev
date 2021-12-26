3012d63b4bb553ea7921174c3405687b9f7c67ff
EdwinMTS <edwinolofmartens@gmail.com>
2021-12-26 23:42:14 +0100

    Number Guesser: finish

--- JS/Number Guesser/script.js
+++ JS/Number Guesser/script.js
@@ -1,3 +1,34 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
+
+function generateTarget () {
+    return Math.floor(Math.random() * 10)
+}
+
+
+function compareGuesses(humanGuess, computerGuess, target) {
+    let humanDistance = Math.abs(humanGuess - target);
+    let computerDistance = Math.abs(computerGuess - target);
+    console.log(Math.abs(humanGuess - target))
+    console.log(Math.abs(computerGuess - target))
+
+    if (humanDistance <= computerDistance) {
+        return true;
+    } else if (computerDistance < humanDistance) {
+        return false;
+    }
+}
+
+function updateScore(winner) {
+    if (winner === "human") {
+        humanScore++;
+    } else if (winner === "computer") {
+        computerScore++;
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber++;
+}
+
